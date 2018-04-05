77bdc5955e560974297bdaee5bd72dfdf8fd4cac
Niranjan Manjunath <niranjansm@localhost.localdomain>
2018-04-05 23:17:00 +0530

    First commit

---
+++ translate.py
@@ -0,0 +1,13 @@
+# Read a german word and translate that using google translate api
+import requests
+from googletrans import Translator
+
+translator = Translator()
+while True:
+	print "Please enter the german word to translate"
+	german_word = raw_input()
+
+	english_word = translator.translate(german_word, src='de')
+	print "English Translation: " + english_word.text
+	print ""  
+
