f74bb749dcea1f40ddac1e09b49445a671298929
Niranjan Manjunath <niranjansm@localhost.localdomain>
2018-04-09 22:31:57 +0530

    code modified for Python3

--- translate.py
+++ translate.py
@@ -1,13 +1,17 @@
 # Read a german word and translate that using google translate api
 import requests
 from googletrans import Translator
+from datetime import datetime
+
+now = datetime.now()
+print (str(now))
 
 translator = Translator()
 while True:
-	print "Please enter the german word to translate"
+	print("Please enter the german word to translate")
 	german_word = raw_input()
 
 	english_word = translator.translate(german_word, src='de')
-	print "English Translation: " + english_word.text
-	print ""  
+	print("English Translation: " + english_word.text)
+	print("")
 
